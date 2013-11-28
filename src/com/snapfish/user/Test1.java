92113e8395a914f70e6d861efb6eabcd0938a0eb
npavan <pavanam.kumar.n@gmail.com>
2013-11-28 17:29:31 +0530

    Update Test1.java

--- src/com/snapfish/user/Test1.java
+++ src/com/snapfish/user/Test1.java
@@ -2,7 +2,7 @@ package com.snapfish.user;
 
 public class Test1 {
   public static void main(String args[]){
-	  System.out.println("Hi Pavan2gsdfgsdfsdaf");
+
 
 	  System.out.println("Hi Pavan2gsdfgsdfsdaf");
 	  System.out.println("Hi Pav2342an2gsdf");
