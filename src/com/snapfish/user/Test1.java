6339305d1ef8fa48a5354b7343a78f48f5d4591a
npavan <pavanam.kumar.n@gmail.com>
2013-11-28 16:29:30 +0530

    asdf

--- src/com/snapfish/user/Test1.java
+++ src/com/snapfish/user/Test1.java
@@ -4,8 +4,7 @@ public class Test1 {
   public static void main(String args[]){
 	  System.out.println("Hi Pavan2gsdfgsdfsdaf");
 
-	  System.out.println("Hi Pavan2gsdf");
-
 	  System.out.println("Hi Pavan2gsdfgsdfsdaf");
+	  System.out.println("Hi Pavan2gsdf");
   }
 }
